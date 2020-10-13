4f8bc81ec4eef71cf687f993d2fd466de9ab4c7c
Padmavathi Betala <58491043+vempadiP@users.noreply.github.com>
2020-10-13 23:51:22 +0200

    Create JSONObjectRootLevel2Class.java

---
+++ src/main/java/demoFnal/JSONObjectRootLevel2Class.java
@@ -0,0 +1,41 @@
+//***********************************************************************
+// Revision History
+// Class      : JSONObjectRootLevel2Class
+// Date       : 01/05/2020
+// Name       : Padmavathi Vempadi
+// Description: The definition of this Java class  is created to hold/represent the level 2 structure Repository of an JSON object.
+// Parameters : N/A
+//***********************************************************************
+
+package demoFnl;
+
+public class JSONObjectRootLevel2Class {
+	
+	private int id;	
+	private String node_id;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNode_id() {
+		return node_id;
+	}
+	public void setNode_id(String node_id) {
+		this.node_id = node_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+	
+	
+	
+}
